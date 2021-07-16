37accf70818187d7e2d6165e0924b681d39f852f
Keijiro Takahashi <keijiro@gmail.com>
2021-07-17 00:44:30 +0900

    Tweaks

--- Assets/Visualizer.cs
+++ Assets/Visualizer.cs
@@ -7,13 +7,14 @@ sealed class Visualizer : MonoBehaviour
 {
     [SerializeField] ImageSource _source = null;
     [SerializeField] ResourceSet _resources = null;
+    [SerializeField] Vector2Int _resolution = new Vector2Int(512, 384);
     [SerializeField] RawImage _previewUI = null;
     [SerializeField] RawImage _stencilUI = null;
 
     BodyPixRuntime _bodypix;
 
     void Start()
-      => _bodypix = new BodyPixRuntime(_resources, 400, 300);
+      => _bodypix = new BodyPixRuntime(_resources, _resolution.x, _resolution.y);
 
     void OnDestroy()
       => _bodypix.Dispose();
