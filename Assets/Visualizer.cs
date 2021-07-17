fd38da3a7dc45ed3a6caf801e6d41d2879cbf73d
Keijiro Takahashi <keijiro@gmail.com>
2021-07-17 12:01:39 +0900

    Pose visualization

--- Assets/Visualizer.cs
+++ Assets/Visualizer.cs
@@ -10,21 +10,23 @@ sealed class Visualizer : MonoBehaviour
     [SerializeField] Vector2Int _resolution = new Vector2Int(512, 384);
     [SerializeField] RawImage _previewUI = null;
     [SerializeField] RawImage _stencilUI = null;
-    [SerializeField] Shader _shader = null;
+    [SerializeField] Shader _keypointShader = null;
+    [SerializeField] Shader _boneShader = null;
 
     BodyPixRuntime _bodypix;
-    Material _material;
+    (Material keypoint, Material bone) _material;
 
     void Start()
     {
         _bodypix = new BodyPixRuntime(_resources, _resolution.x, _resolution.y);
-        _material = new Material(_shader);
+        _material = (new Material(_keypointShader), new Material(_boneShader));
     }
 
     void OnDestroy()
     {
         _bodypix.Dispose();
-        Destroy(_material);
+        Destroy(_material.keypoint);
+        Destroy(_material.bone);
     }
 
     void LateUpdate()
@@ -33,11 +35,18 @@ sealed class Visualizer : MonoBehaviour
         _previewUI.texture = _source.Texture;
         _stencilUI.texture = _bodypix.Stencil;
 
-        _material.SetBuffer("_Keypoints", _bodypix.Keypoints);
-        _material.SetFloat("_Aspect", (float)_resolution.x / _resolution.y);
+        _material.keypoint.SetBuffer("_Keypoints", _bodypix.Keypoints);
+        _material.keypoint.SetFloat("_Aspect", (float)_resolution.x / _resolution.y);
+
+        _material.bone.SetBuffer("_Keypoints", _bodypix.Keypoints);
+        _material.bone.SetFloat("_Aspect", (float)_resolution.x / _resolution.y);
 
         Graphics.DrawProcedural
-          (_material, new Bounds(Vector3.zero, Vector3.one),
+          (_material.keypoint, new Bounds(Vector3.zero, Vector3.one),
            MeshTopology.Triangles, 6, BodyPixRuntime.PartCount);
+
+        Graphics.DrawProcedural
+          (_material.bone, new Bounds(Vector3.zero, Vector3.one),
+           MeshTopology.Lines, 2, 12);
     }
 }
--- Packages/jp.keijiro.bodypix/Shader/Common.hlsl
+++ Packages/jp.keijiro.bodypix/Shader/Common.hlsl
@@ -1,7 +1,25 @@
 #ifndef _BODYPIXBARRACUDA_COMMON_H_
 #define _BODYPIXBARRACUDA_COMMON_H_
 
-#define PART_COUNT 17
+#define PART_NOSE           0
+#define PART_LEFT_EYE       1
+#define PART_RIGHT_EYE      2
+#define PART_LEFT_EAR       3
+#define PART_RIGHT_EAR      4
+#define PART_LEFT_SHOULDER  5
+#define PART_RIGHT_SHOULDER 6
+#define PART_LEFT_ELBOW     7
+#define PART_RIGHT_ELBOW    8
+#define PART_LEFT_WRIST     9
+#define PART_RIGHT_WRIST    10
+#define PART_LEFT_HIP       11
+#define PART_RIGHT_HIP      12
+#define PART_LEFT_KNEE      13
+#define PART_RIGHT_KNEE     14
+#define PART_LEFT_ANKLE     15
+#define PART_RIGHT_ANKLE    16
+
+#define PART_COUNT          17
 
 float Sigmoid(float x)
 {
