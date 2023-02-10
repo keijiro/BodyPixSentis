8c72512a5892e420084385bde78c3f83037d392f
Keijiro Takahashi <keijiro@gmail.com>
2023-02-10 18:02:38 +0900

    Directly peek output buffers instead of copying into temp RTs

--- Packages/jp.keijiro.bodypix/Script/BodyDetector.cs
+++ Packages/jp.keijiro.bodypix/Script/BodyDetector.cs
@@ -21,10 +21,10 @@ public sealed class BodyDetector : System.IDisposable
       => _readCache.Cached;
 
     public RenderTexture MaskTexture
-      => _buffers.mask;
+      => _output.mask;
 
     public GraphicsBuffer KeypointBuffer
-      => _buffers.keypoints;
+      => _output.keypoints;
 
     #endregion
 
@@ -33,62 +33,39 @@ public sealed class BodyDetector : System.IDisposable
     ResourceSet _resources;
     Config _config;
     IWorker _worker;
-
     (Tensor tensor, ComputeTensorData data) _preprocess;
-
-    (RenderTexture segment,
-     RenderTexture parts,
-     RenderTexture heatmaps,
-     RenderTexture offsets,
-     RenderTexture mask,
-     GraphicsBuffer keypoints) _buffers;
-
+    (RenderTexture mask, GraphicsBuffer keypoints) _output;
     KeypointCache _readCache;
 
     void AllocateObjects(ResourceSet resources, int width, int height)
     {
-        // NN model loading
+        // NN model
         var model = ModelLoader.Load(resources.model);
 
-        // Private object initialization
+        // Private objects
         _resources = resources;
         _config = new Config(model, _resources, width, height);
         _worker = model.CreateWorker(WorkerFactory.Device.GPU);
 
-        // Buffer allocation
+        // Preprocessing buffers
 #if BARRACUDA_4_0_0_OR_LATER
-        _preprocess.data = new ComputeTensorData
-          (_config.InputShape, "Preprocess", false);
+        _preprocess.data =
+          new ComputeTensorData(_config.InputShape, "Preprocess", false);
         _preprocess.tensor = TensorFloat.Zeros(_config.InputShape);
         _preprocess.tensor.AttachToDevice(_preprocess.data);
 #else
-        _preprocess.data = new ComputeTensorData
-          (_config.InputShape, "Preprocess",
-           ComputeInfo.ChannelsOrder.NHWC, false);
+        _preprocess.data =
+          new ComputeTensorData(_config.InputShape, "Preprocess",
+                                ComputeInfo.ChannelsOrder.NHWC, false);
         _preprocess.tensor = new Tensor(_config.InputShape, _preprocess.data);
 #endif
 
-        _buffers.segment = RTUtil.NewFloat
-          (_config.OutputWidth, _config.OutputHeight);
+        // Output buffers
+        _output.mask = BufferUtil.NewArgbUav(_config.OutputWidth, _config.OutputHeight);
+        _output.keypoints = BufferUtil.NewFloat4(Body.KeypointCount);
 
-        _buffers.parts = RTUtil.NewFloat
-          (_config.OutputWidth * 24, _config.OutputHeight);
-
-        _buffers.heatmaps = RTUtil.NewFloat
-          (_config.OutputWidth * Body.KeypointCount, _config.OutputHeight);
-
-        _buffers.offsets = RTUtil.NewFloat
-          (_config.OutputWidth * Body.KeypointCount * 2, _config.OutputHeight);
-
-        _buffers.mask = RTUtil.NewArgbUav
-          (_config.OutputWidth, _config.OutputHeight);
-
-        _buffers.keypoints = new GraphicsBuffer
-          (GraphicsBuffer.Target.Structured,
-           Body.KeypointCount, sizeof(float) * 4);
-
-        // Keypoint data read cache initialization
-        _readCache = new KeypointCache(_buffers.keypoints);
+        // Keypoint read cache
+        _readCache = new KeypointCache(_output.keypoints);
     }
 
     void DeallocateObjects()
@@ -99,23 +76,11 @@ public sealed class BodyDetector : System.IDisposable
         _preprocess.tensor?.Dispose();
         _preprocess = (null, null);
 
-        ObjectUtil.Destroy(_buffers.segment);
-        _buffers.segment = null;
+        BufferUtil.Destroy(_output.mask);
+        _output.mask = null;
 
-        ObjectUtil.Destroy(_buffers.parts);
-        _buffers.parts = null;
-
-        ObjectUtil.Destroy(_buffers.heatmaps);
-        _buffers.heatmaps = null;
-
-        ObjectUtil.Destroy(_buffers.offsets);
-        _buffers.offsets = null;
-
-        ObjectUtil.Destroy(_buffers.mask);
-        _buffers.mask = null;
-
-        _buffers.keypoints?.Dispose();
-        _buffers.keypoints = null;
+        _output.keypoints?.Dispose();
+        _output.keypoints = null;
     }
 
     #endregion
@@ -136,27 +101,21 @@ public sealed class BodyDetector : System.IDisposable
         // NN worker invocation
         _worker.Execute(_preprocess.tensor);
 
-        // NN output retrieval
-        _worker.CopyOutput("segments", _buffers.segment);
-        _worker.CopyOutput("part_heatmaps", _buffers.parts);
-        _worker.CopyOutput("heatmaps", _buffers.heatmaps);
-        _worker.CopyOutput("short_offsets", _buffers.offsets);
-
         // Postprocessing (mask)
         var post1 = _resources.mask;
-        post1.SetTexture(0, "Segments", _buffers.segment);
-        post1.SetTexture(0, "Heatmaps", _buffers.parts);
-        post1.SetTexture(0, "Output", _buffers.mask);
+        post1.SetBuffer(0, "Segments", _worker.PeekOutputBuffer("segments"));
+        post1.SetBuffer(0, "Heatmaps", _worker.PeekOutputBuffer("part_heatmaps"));
+        post1.SetTexture(0, "Output", _output.mask);
         post1.SetInts("InputSize", _config.OutputWidth, _config.OutputHeight);
         post1.DispatchThreads(0, _config.OutputWidth, _config.OutputHeight, 1);
 
         // Postprocessing (keypoints)
         var post2 = _resources.keypoints;
-        post2.SetTexture(0, "Heatmaps", _buffers.heatmaps);
-        post2.SetTexture(0, "Offsets", _buffers.offsets);
+        post2.SetBuffer(0, "Heatmaps", _worker.PeekOutputBuffer("heatmaps"));
+        post2.SetBuffer(0, "Offsets", _worker.PeekOutputBuffer("short_offsets"));
         post2.SetInts("InputSize", _config.OutputWidth, _config.OutputHeight);
         post2.SetInt("Stride", _config.Stride);
-        post2.SetBuffer(0, "Keypoints", _buffers.keypoints);
+        post2.SetBuffer(0, "Keypoints", _output.keypoints);
         post2.Dispatch(0, 1, 1, 1);
 
         // Cache data invalidation
--- Packages/jp.keijiro.bodypix/Script/Internal/Utility.cs
+++ Packages/jp.keijiro.bodypix/Script/Internal/Utility.cs
@@ -5,8 +5,20 @@ namespace BodyPix {
 
 #region Object construction/destruction helpers
 
-static class ObjectUtil
+static class BufferUtil
 {
+    public static RenderTexture NewArgbUav(int w, int h)
+    {
+        var rt = new RenderTexture(w, h, 0, RenderTextureFormat.Default,
+                                            RenderTextureReadWrite.Linear);
+        rt.enableRandomWrite = true;
+        rt.Create();
+        return rt;
+    }
+
+    public static GraphicsBuffer NewFloat4(int length)
+      => new GraphicsBuffer(GraphicsBuffer.Target.Structured, length, sizeof(float) * 4);
+
     public static void Destroy(Object o)
     {
         if (o == null) return;
@@ -14,21 +26,6 @@ static class ObjectUtil
             Object.Destroy(o);
         else
             Object.DestroyImmediate(o);
-    }
-}
-
-static class RTUtil
-{
-    public static RenderTexture NewFloat(int w, int h)
-      => new RenderTexture(w, h, 0, RenderTextureFormat.RFloat);
-
-    public static RenderTexture NewArgbUav(int w, int h)
-    {
-        var rt = new RenderTexture
-          (w, h, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Linear);
-        rt.enableRandomWrite = true;
-        rt.Create();
-        return rt;
     }
 }
 
@@ -49,31 +46,18 @@ static class ComputeShaderExtensions
     {
         uint xc, yc, zc;
         compute.GetKernelThreadGroupSizes(kernel, out xc, out yc, out zc);
-
         x = (x + (int)xc - 1) / (int)xc;
         y = (y + (int)yc - 1) / (int)yc;
         z = (z + (int)zc - 1) / (int)zc;
-
         compute.Dispatch(kernel, x, y, z);
     }
 }
 
 static class IWorkerExtensions
 {
-    public static void CopyOutput
-      (this IWorker worker, string tensorName, RenderTexture rt)
-    {
-        var output = worker.PeekOutput(tensorName);
-#if BARRACUDA_4_0_0_OR_LATER
-        var shape = new TensorShape(1, 1, rt.height, rt.width);
-        using var tensor = output.ShallowReshape(shape);
-        TensorToRenderTexture.ToRenderTexture((TensorFloat)tensor, rt);
-#else
-        var shape = new TensorShape(1, rt.height, rt.width, 1);
-        using var tensor = output.Reshape(shape);
-        tensor.ToRenderTexture(rt);
-#endif
-    }
+    public static ComputeBuffer PeekOutputBuffer
+      (this IWorker worker, string tensorName)
+      => ((ComputeTensorData)worker.PeekOutput(tensorName).tensorOnDevice).buffer;
 }
 
 #endregion
